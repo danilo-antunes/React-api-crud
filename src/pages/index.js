c38b30e14ec67d569c8c029c365a306eabfa23d4
danilo-antunes <danilo.fe.antunes@gmail.com>
2023-05-23 15:00:49 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ const Page = () => {
           <Stack spacing={3}>
             <Stack direction="row" justifyContent="space-between" spacing={4}>
               <Stack spacing={1}>
-                <Typography variant="h3">Meters</Typography>
+                <Typography variant="h3">METERs</Typography>
                 <Stack alignItems="center" direction="row" spacing={1}></Stack>
               </Stack>
               <div>
