d841682b363f421d5fdff274b913dd8417f0c8b9
danilo-antunes <danilo.fe.antunes@gmail.com>
2023-05-23 15:00:07 -0300

    Update index.js
    change name from APIs to METERs

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ const Page = () => {
           <Stack spacing={3}>
             <Stack direction="row" justifyContent="space-between" spacing={4}>
               <Stack spacing={1}>
-                <Typography variant="h3">APIs</Typography>
+                <Typography variant="h3">Meters</Typography>
                 <Stack alignItems="center" direction="row" spacing={1}></Stack>
               </Stack>
               <div>
